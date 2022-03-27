d79f4dfae8c9b876a12cf87e7853425317cea0b3
sherzod02 <kholmamatovsh09>
2022-03-27 21:19:45 +0500

    good, cheap, fast cheackboxes

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const toggles = document.querySelectorAll(".toggle");
+const good = document.querySelector("#good");
+const cheap = document.querySelector("#cheap");
+const fast = document.querySelector("#fast");
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => {
+    doTheTrict(e.target);
+  })
+);
+
+function doTheTrict(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
